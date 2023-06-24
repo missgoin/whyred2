930b5892ad076e31ce0f61bd5214e620827d2c59
missgoin <mister.goindo@gmail.com>
2023-06-24 11:44:48 +0700

    -3+3

--- .circleci/whyred.sh
+++ .circleci/whyred.sh
@@ -11,7 +11,7 @@ KERNEL_DIR="$(pwd)"
 ##----------------------------------------------------------##
 # Device Name and Model
 MODEL=Xiaomi
-DEVICE=lavender
+DEVICE=tulip
 
 # Kernel Version Code
 #VERSION=
